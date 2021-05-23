4551e8afc6760ec6a31aaed392bcd60a48562ae1
Martin Wong <martinwong327@gmail.com>
2021-05-23 15:52:53 +0800

    save accept socket

--- testSocket/src/main.cpp
+++ testSocket/src/main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <cstring>
 #include "yaml-cpp/yaml.h"
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 class Endpoint {
     std::string host_;
@@ -16,6 +18,8 @@ public:
     short getPort() { return port_; }
 };
 
+class asio;
+
 bool hasNextArgument(int argc, int i) {
     return (argc - i > 1);
 }
@@ -24,10 +28,20 @@ bool isStringSame(const char *str1, const char *str2) {
     return (std::strcmp(str1, str2) == 0);
 }
 
+void printFunc() {
+    std::cout << "Accepted Connection" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
     std::string configPath;
     std::shared_ptr<Endpoint> serverEndpoint;
     std::vector<std::shared_ptr<Endpoint>> clientEndpoints;
+    bool notExit = true;
+
+    if (argc < 2) {
+        std::cout << "no arguments, exiting" << std::endl;
+        return 0;
+    }
 
     for (int i = 0; i < argc; ++i) {
         if (isStringSame(argv[i], "--configPath") && hasNextArgument(argc, i)) {
@@ -60,7 +74,37 @@ int main(int argc, char *argv[]) {
 
     std::cout << "finished loading config" << std::endl;
 
+    try {
+        boost::asio::io_service ioService;
+        boost::asio::ip::tcp::acceptor acceptor(ioService, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), serverEndpoint->getPort()));
+        boost::system::error_code ignore_error;
+        boost::asio::streambuf buffer;
+        size_t buf_size = 20, headerBufferSize = 4;
+        std::vector<std::shared_ptr<boost::asio::ip::tcp::socket>> clientSocketsList;
 
+        std::cout << "Building server socket" << std::endl;
+        boost::asio::ip::tcp::socket serverSocket(ioService);
+
+        std::cout << "Building client socket" << std::endl;
+        boost::asio::ip::tcp::socket clientSocket(ioService);
+
+        std::cout << "Accept to socket" << std::endl;
+        acceptor.async_accept(serverSocket, boost::bind(&printFunc));
+        ioService.run();
+
+        while (notExit) {
+            size_t bytesTransferred = boost::asio::read(serverSocket, buffer, boost::asio::transfer_exactly(buf_size));
+            boost::asio::streambuf::const_buffers_type bufs = buffer.data();
+            std::cout << "Read [" << std::string(boost::asio::buffers_begin(bufs), boost::asio::buffers_begin(bufs) + buf_size) << "]" << std::endl;
+        }
+
+//        while (true) {
+//            boost::asio::read(socket, buffer, ignore_error);
+//        }
+
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
