be42c05c1883d8be3a1bf920bba732b2271b4af4
Martin Wong <martinwong327@gmail.com>
2023-04-05 10:44:15 +0800

    move folder

--- random/src/socket_comm_client.hpp
+++
@@ -1,9 +0,0 @@
-#include "socket_comm.hpp"
-
-class socket_comm_client : public socket_comm {
-    boost::asio::ip::tcp::socket socket_;
-public:
-    socket_comm_client(const std::string& host, int port);
-    void run() override;
-    ~socket_comm_client() override;
-};
